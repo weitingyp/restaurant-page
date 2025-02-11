1ac9940b8bb241dee915c8efd42e1c8536611c5a
Wei-Ting Yap <29475856+weitingyap@users.noreply.github.com>
2025-02-11 15:13:46 +0800

    feat: load home and about page when nav buttons are clicked

---
+++ src/about.js
@@ -0,0 +1,6 @@
+const aboutPage = document.createElement("div");
+aboutPage.className = "page";
+
+aboutPage.innerText = "about";
+
+export default aboutPage;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 import homePage from "./home.js";
+import aboutPage from "./about.js";
 import "./styles.css";
 
 const content = document.querySelector("div#content");
+
+// Page Navigation Buttons
+const homeBtn = document.querySelector("#nav-home-btn");
+const menuBtn = document.querySelector("#nav-menu-btn");
+const aboutBtn = document.querySelector("#nav-about-btn");
+
+// Load home by default
 content.appendChild(homePage);
-console.log(homePage);
+
+// Clear container
+const resetContent = () => {content.innerHTML = ""};
+
+// Load home on button 
+const renderHome = ()=>{
+    resetContent();
+    content.appendChild(homePage);
+}
+
+homeBtn.addEventListener('click', renderHome);
+
+// Load about on button click 
+const renderAbout = ()=>{
+    resetContent();
+    content.appendChild(aboutPage);
+}
+
+aboutBtn.addEventListener('click', renderAbout);
