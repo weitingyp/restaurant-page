9e895f3f7ac060d313447b4069846567a43967be
Wei-Ting Yap <29475856+weitingyap@users.noreply.github.com>
2025-02-11 14:01:19 +0800

    feat: add style-loader and css-loader packages

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,10 @@ module.exports = {
   module: {
     rules: [
         {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
             test: /\.html$/i,
             loader: "html-loader",
           },
